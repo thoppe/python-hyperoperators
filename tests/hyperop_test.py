8b5476a95b8b0e2e6d23f387c9634152d4d5d8ca
thoppe <travis.hoppe@gmail.com>
2016-02-07 18:38:42 -0500

    Refactor test suite to remove code reuse

--- tests/hyperop_test.py
+++ tests/hyperop_test.py
@@ -1,51 +1,38 @@
-import unittest, itertools, math
+import unittest, itertools, math, operator
 from hyperop import hyperop
 
 testing_values = range(1,15)
 
-#def generic_evaulation(fA, fB):
-#    for x,y in itertools.product(testing_values,repeat=2):
-#        assert( fA(x,y) == fB(x,y) )
+def check_range(fA, fB):
+    for x,y in itertools.product(testing_values,repeat=2):
+        assert( fA(x,y) == fB(x,y) )
 
 class PrimitiveSmallValues(unittest.TestCase):
 
     def test_primitive_H1(self):
-        H = hyperop(1,primitive=True)
-        
-        for a,b in itertools.product(testing_values,repeat=2):
-            assert( H(a,b) == a+b )
+        check_range(hyperop(1,primitive=True), operator.add)
 
     def test_primitive_H2(self):
-        H = hyperop(2,primitive=True)
-        for a,b in itertools.product(testing_values,repeat=2):
-            assert( H(a,b) == a*b )
+        check_range(hyperop(2,primitive=True), operator.mul)
 
     def test_primitive_H3(self):
-        H = hyperop(3,primitive=True)
-        for a,b in itertools.product(testing_values,repeat=2):
-            assert( H(a,b) == a**b )
+        check_range(hyperop(3,primitive=True), operator.pow)
+        
 
 class SmallValues(unittest.TestCase):
 
     def test_H0(self):
-        H = hyperop(0)
-        for a,b in itertools.product(testing_values,repeat=2):
-            assert( H(a,b) == 1+b )
+        successor = lambda a,b: 1+b
+        check_range(hyperop(0), successor)
     
     def test_H1(self):
-        H = hyperop(1)
-        for a,b in itertools.product(testing_values,repeat=2):
-            assert( H(a,b) == a+b )
+        check_range(hyperop(1), operator.add)
             
     def test_H2(self):
-        H = hyperop(2)
-        for a,b in itertools.product(testing_values,repeat=2):
-            assert( H(a,b) == a*b )
+        check_range(hyperop(2), operator.mul)
 
     def test_H3(self):
-        H = hyperop(3)
-        for a,b in itertools.product(testing_values,repeat=2):
-            assert( H(a,b) == a**b )
+        check_range(hyperop(3), operator.pow)
 
 class KnownValues(unittest.TestCase):
     def test_H4(self):
