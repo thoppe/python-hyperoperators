e299b7ee68bbbd3d72b2042dac1b9acf59eee671
thoppe <travis.hoppe@gmail.com>
2016-02-11 16:19:57 -0500

    autopep8 status

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ setup(
     name="hyperop",
     packages=['hyperop'],
     version=__version__,
-    download_url = 'https://github.com/thoppe/python-hyperoperators/tarball/1.0',
+    download_url='https://github.com/thoppe/python-hyperoperators/tarball/1.0',
     author="Travis Hoppe",
     author_email="travis.hoppe+hyperop@gmail.com",
     description=(
         "Hyperoperators (succession, addition, multiplication, exponentiation, tetration and higher) in python."),
     license = "Creative Commons Attribution-ShareAlike 4.0 International License",
-    keywords = ["math","hyperoperators","uparrow","large-numbers",],
+    keywords = ["math", "hyperoperators", "uparrow", "large-numbers", ],
     url="https://github.com/thoppe/python-hyperoperators",
     test_suite="tests",
 )
